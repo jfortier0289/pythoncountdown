f2e9c3db37fada12cbc3925e8dda25cb5a3f11cc
jfortier0289 <47324830+jfortier0289@users.noreply.github.com>
2019-02-04 15:13:25 -0500

    Update datecountdown.py

--- datecountdown.py
+++ datecountdown.py
@@ -5,6 +5,10 @@ def diff_dates(date1, date2):
 
 def main():
     d1 = date.today()
+###########################################################
+#   Below is where is change the date where the countdown #
+#   is to                                                 #
+###########################################################
     d2 = date(2019,12,25)
     result1 = diff_dates(d2, d1)
     print '{} days b4 christmas'.format(result1, d1, d2)
